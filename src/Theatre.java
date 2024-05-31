44a7b3bb88b0e9c9c95bd713a23b710841552126
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-31 08:07:10 +0530

    first commit

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,4 +1,9 @@
 public class Theatre {
+
+    static int[] row1=new int[12];
+    static int[] row2=new int[16];
+    static int[] row3=new int[20];
+
     public static void main(String[] args) {
 
     }
