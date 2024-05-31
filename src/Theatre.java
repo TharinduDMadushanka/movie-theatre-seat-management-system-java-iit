ce1146e3d30cdf425fb3f946c3e7fe0cbf3073a9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-31 08:11:11 +0530

    first commit

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,21 +1,25 @@
 public class Theatre {
 
-    static int[] row1=new int[12];
-    static int[] row2=new int[16];
-    static int[] row3=new int[20];
+    static int[] row1 = new int[12];
+    static int[] row2 = new int[16];
+    static int[] row3 = new int[20];
 
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO THE NEW THEATRE");
     }
 
-    public static void menuStuff(){
+    public static void menuStuff() {
 
-        for (int i=0;i<30;i++){
+        for (int i = 0; i < 30; i++) {
             System.out.println("-");
         }
     }
-    public static void menu(){
 
+    public static void menu() {
+
+        menuStuff();
+        System.out.println("|OPTION MEU|");
+        menuStuff();
     }
 }
